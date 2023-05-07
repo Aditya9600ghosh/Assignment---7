31f3564726e5b033e0eb4ffbc1c05e6453c3b88f
Aditya9600ghosh <130989727+Aditya9600ghosh@users.noreply.github.com>
2023-05-07 21:53:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var a=0
+function change(){
+    let b=["red","green","yellow","orange","pink","purple","aquablue","dogerblue"]
+    if(a > b.length-1){
+        a=0
+    }
+    document.getElementsByTagName("body")[0].style.backgroundColor=b[a++];
+}
